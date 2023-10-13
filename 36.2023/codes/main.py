5a31b1e6056dfd845fc635579347291b80937e3f
Sai Kowshik Padala <106828551+saikowshikp@users.noreply.github.com>
2023-10-13 22:23:35 +0530

    Add files via upload

---
+++ 36.2023/codes/main.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+
+# Define the points
+points = [(-3, -1), (-2, -1), (-1, 0), (1, 0), (2, 1), (3, 1)]
+
+# Extract x and y coordinates from the points
+x_coords, y_coords = zip(*points)
+
+# Plot the line segments without points
+plt.plot(x_coords, y_coords, linestyle='-', linewidth=2)
+
+# Set axis labels and title
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('Y=h(X)')
+
+# Add bold X and Y axes
+plt.axhline(0, color='black', linewidth=1)
+plt.axvline(0, color='black', linewidth=1)
+
+# Display the plot
+#plt.grid(True)
+plt.show()
